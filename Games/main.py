86949c29b4782d62dd239e15d7b03eb3b6b6b440
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-02-10 15:42:57 +0100

    commit

--- Games/main.py
+++ Games/main.py
@@ -12,17 +12,16 @@ context.verify_mode = ssl.CERT_NONE
 connection = sqlite3.connect('db.sqlite3')
 cur = connection.cursor()
 cur.execute('DROP TABLE IF EXISTS Games')
-cur.execute('''CREATE TABLE IF NOT EXISTS Games(id TEXT, title TEXT, thumbnail TEXT, short_description TEXT, game_url TEXT, genre TEXT,
+cur.execute('''CREATE TABLE IF NOT EXISTS Games(id TEXT, title TEXT, thumbnail TEXT,
+            short_description TEXT, game_url TEXT, genre TEXT,
             platform TEXT, publisher TEXT, developer TEXT, release_date TEXT);''')
-
+            
 api_endpoint = 'https://www.freetogame.com/api/games'
-response_data = urllib.request.urlopen(api_endpoint, context)
+response_data = urllib.request.urlopen(api_endpoint, context=context)
 data = response_data.read().decode()
-
 json_data = json.loads(data)
-json_dump = json.dumps(json_data, indent=4)
-print (json_dump)
-
+#json_dump = json.dumps(json_data, indent=4)
+#print (json_dump)
 
 for game in json_data:
     id = game['id']
@@ -36,6 +35,7 @@ for game in json_data:
     developer = game['developer']
     release_date = game['release_date']
 
-    cur.execute('INSERT INTO Games VALUES (?,?,?,?,?,?,?,?,?,?)', (id, title, thumbnail, short_description, game_url, genre,
+    cur.execute('INSERT INTO Games VALUES (?,?,?,?,?,?,?,?,?,?)', 
+                (id, title, thumbnail, short_description, game_url, genre,
     platform, publisher, developer, release_date,))
 connection.commit()
