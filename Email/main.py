c2003d78aef30595ee14ab43655d882fb6721e96
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-02-10 14:52:03 +0100

    renamed variables in main.py to follow PEP 8 guidelines

--- Email/main.py
+++ Email/main.py
@@ -1,25 +1,25 @@
 import sqlite3
 
-connection = sqlite3.connect('emaildb.sqlite')
+connection = sqlite3.connect('db.sqlite3')
 cur = connection.cursor()
 
 cur.execute('DROP TABLE IF EXISTS Counts')
 cur.execute ('CREATE TABLE Counts(email TEXT, count INTEGER)')
 
 file_name = input ('Input your file name: ')
-f_handle = open(file_name)
-for line in f_handle:
+file_handle = open(file_name)
+for line in file_handle:
     if not line.startswith('From') :
         continue 
     line = line.strip()
     line_list = line.split()
-    email = line_list[1]
-    cur.execute('SELECT count FROM Counts WHERE email = ?', (email,))
+    email_address = line_list[1]
+    cur.execute('SELECT count FROM Counts WHERE email = ?', (email_address,))
     row = cur.fetchone()
     if row is None :
-        cur.execute('INSERT INTO Counts (email,count)VALUES (?,1)', (email,))
+        cur.execute('INSERT INTO Counts (email,count)VALUES (?,1)', (email_address,))
     else :
         cur.execute('''UPDATE Counts SET count = count + 1
-         WHERE email = ?''', (email,))
+         WHERE email = ?''', (email_address,))
     connection.commit()
 
