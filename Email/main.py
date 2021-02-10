cb824f9845ac844297af6f336c0040f685ed7a1a
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-02-10 15:46:43 +0100

    refactored code

--- Email/main.py
+++ Email/main.py
@@ -2,7 +2,6 @@ import sqlite3
 
 connection = sqlite3.connect('db.sqlite3')
 cur = connection.cursor()
-
 cur.execute('DROP TABLE IF EXISTS Counts')
 cur.execute ('CREATE TABLE Counts(email TEXT, count INTEGER)')
 
@@ -15,6 +14,7 @@ for line in file_handle:
     email_address = line_list[1]
     cur.execute('SELECT count FROM Counts WHERE email = ?', (email_address,))
     row = cur.fetchone()
+    
     if row is None :
         cur.execute('INSERT INTO Counts (email,count)VALUES (?,1)', (email_address,))
     else :
--- Games/main.py
+++ Games/main.py
@@ -3,7 +3,6 @@ import ssl
 import urllib.request
 import sqlite3
 
-
 # Ignore SSL certificate errors
 context = ssl.create_default_context()
 context.check_hostname = False
@@ -15,7 +14,7 @@ cur.execute('DROP TABLE IF EXISTS Games')
 cur.execute('''CREATE TABLE IF NOT EXISTS Games(id TEXT, title TEXT, thumbnail TEXT,
             short_description TEXT, game_url TEXT, genre TEXT,
             platform TEXT, publisher TEXT, developer TEXT, release_date TEXT);''')
-            
+
 api_endpoint = 'https://www.freetogame.com/api/games'
 response_data = urllib.request.urlopen(api_endpoint, context=context)
 data = response_data.read().decode()
